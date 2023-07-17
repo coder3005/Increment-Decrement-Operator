d3b6138abedeb20334be0a9b92ec7f4d2347ae72
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-17 10:56:45 +0530

    Create PrePost1.java

---
+++ PrePost1.java
@@ -0,0 +1,16 @@
+/* Incr/Decr Operator Example...! */
+class PrePost
+{
+    public static void main(String[] args) {
+        
+        int a=10;
+        System.out.println(a);
+        System.out.println("PrePost Increment.. ");
+        System.out.println(a++);
+        System.out.println(++a);
+
+        System.out.println("PrePost Decrement.. ");
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}
